58f9a86ce55ed51a5d7cdcf589718b759f692a6a
Peter Halasz <pete.halasz@gmail.com>
2016-03-31 10:59:29 +0100

    Add support for the new eZRichText field type.

---
+++ Core/API/ComplexFields/EzRichText.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Kaliop\eZMigrationBundle\Core\API\ComplexFields;
+
+
+class EzRichText extends AbstractComplexField
+{
+    /**
+     * Replace any references in an xml string to be used as the input data for an ezrichtext field.
+     *
+     * @return string
+     */
+    public function createValue()
+    {
+        $xmlText = $this->fieldValueArray['content'];
+
+        //Check if there are any references in the xml text and replace them.
+        // $result[0][] will have the matched full string eg.: [reference:example_reference]
+        // $result[1][] will have the reference id eg.: example_reference
+        $count = preg_match_all('|\[reference:([^\]\[]*)\]|', $xmlText, $result);
+
+        if ($count !== false and count($result) > 1) {
+            foreach ($result[1] as $index => $referenceIdentifier) {
+                $reference = $this->contentManager->getReference($referenceIdentifier);
+
+                $xmlText = str_replace($result[0][$index], $reference, $xmlText);
+            }
+        }
+
+        return $xmlText;
+    }
+}
