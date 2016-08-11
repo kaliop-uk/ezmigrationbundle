d3d1146bc1710eed689b42f1dfcba0f3cf809773
gggeek <ggiunta@kaliop.com>
2016-08-11 14:20:25 +0100

    add support for references in object-relation and object-relation-list attributes

---
+++ Core/ComplexField/EzRelation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Kaliop\eZMigrationBundle\Core\ComplexField;
+
+use eZ\Publish\Core\FieldType\Relation\Value;
+use Kaliop\eZMigrationBundle\API\ComplexFieldInterface;
+
+class EzRelation extends AbstractComplexField implements ComplexFieldInterface
+{
+    /**
+     * Creates a value object to use as the field value when setting an ez page field type.
+     *
+     * @param array $fieldValueArray The definition of the field value, structured in the yml file
+     * @param array $context The context for execution of the current migrations. Contains f.e. the path to the migration
+     * @return PageValue
+     */
+    public function createValue(array $fieldValueArray, array $context = array())
+    {
+        $id = $fieldValueArray['destinationContentId'];
+
+        if ($this->referenceResolver->isReference($id)) {
+            $id = $this->referenceResolver->getReferenceValue($id);
+        }
+
+        return new Value($id);
+    }
+}
---
+++ Core/ComplexField/EzRelationList.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Kaliop\eZMigrationBundle\Core\ComplexField;
+
+use eZ\Publish\Core\FieldType\RelationList\Value;
+use Kaliop\eZMigrationBundle\API\ComplexFieldInterface;
+
+class EzRelationList extends AbstractComplexField implements ComplexFieldInterface
+{
+    /**
+     * @param array $fieldValueArray The definition of the field value, structured in the yml file
+     * @param array $context The context for execution of the current migrations. Contains f.e. the path to the migration
+     * @return PageValue
+     */
+    public function createValue(array $fieldValueArray, array $context = array())
+    {
+        if (count($fieldValueArray) == 1 && isset($fieldValueArray['destinationContentIds'])) {
+            // fromHash format
+            $ids = $fieldValueArray['destinationContentIds'];
+        } else {
+            // simplified format
+            $ids = $fieldValueArray;
+        }
+
+        foreach ($ids as $key => $id) {
+            if ($this->referenceResolver->isReference($id)) {
+                $ids[$key] = $this->referenceResolver->getReferenceValue($id);
+            }
+        }
+
+        return new Value($ids);
+    }
+}
