d599125354afe098bca25ec5359b53e19bf3d0ca
gggeek <ggiunta@kaliop.com>
2016-10-20 11:32:16 +0100

    Fix previous commit

--- Core/ComplexField/EzImage.php
+++ Core/ComplexField/EzImage.php
@@ -31,7 +31,7 @@ class EzImage extends AbstractComplexField implements ComplexFieldInterface
         }
 
         // default format: path is relative to the 'images' dir
-        $realFilePath = dirname($context['path']) . '/images/' . $filePath['path'];
+        $realFilePath = dirname($context['path']) . '/images/' . $filePath;
 
         // but in the past, when using a string, this worked as well as an absolute path, so we have to support it as well
         if (!is_file($realFilePath) && is_file($filePath)) {
