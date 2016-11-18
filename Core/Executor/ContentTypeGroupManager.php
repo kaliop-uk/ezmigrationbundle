32dbbe70a6d5b84979ada3676409a508f23167c9
gggeek <ggiunta@kaliop.com>
2016-11-18 23:26:12 +0000

    change slightly the way contentTypeGroup deletion is handled, to be more inline with modern coding practices of the extension

--- Core/Executor/ContentTypeGroupManager.php
+++ Core/Executor/ContentTypeGroupManager.php
@@ -2,23 +2,43 @@
 
 namespace Kaliop\eZMigrationBundle\Core\Executor;
 
+use Kaliop\eZMigrationBundle\API\Collection\ContentTypeGroupCollection;
 use eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroup;
+use Kaliop\eZMigrationBundle\Core\Matcher\ContentTypeGroupMatcher;
 
 class ContentTypeGroupManager extends RepositoryExecutor
 {
     protected $supportedStepTypes = array('content_type_group');
     protected $supportedActions = array('create', 'delete');
 
+    /**
+     * @var ContentTypeGroupMatcher
+     */
+    protected $contentTypeGroupMatcher;
+
+    /**
+     * @param ContentTypeGroupMatcher $contentTypeGroupMatcher
+     */
+    public function __construct(ContentTypeGroupMatcher $contentTypeGroupMatcher)
+    {
+        $this->contentTypeGroupMatcher = $contentTypeGroupMatcher;
+    }
+
+    /**
+     * @return \eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroup
+     * @throws \Exception
+     * @todo add support for setting creator id and creation date
+     */
     protected function create()
     {
-        $service = $this->repository->getContentTypeService();
-
         if (!isset($this->dsl['identifier'])) {
             throw new \Exception("The 'identifier' key is required to create a new content type group.");
         }
 
-        $createStruct = $service->newContentTypeGroupCreateStruct($this->dsl['identifier']);
-        $group = $service->createContentTypeGroup($createStruct);
+        $contentTypeService = $this->repository->getContentTypeService();
+
+        $createStruct = $contentTypeService->newContentTypeGroupCreateStruct($this->dsl['identifier']);
+        $group = $contentTypeService->createContentTypeGroup($createStruct);
 
         $this->setReferences($group);
 
@@ -32,20 +52,46 @@ class ContentTypeGroupManager extends RepositoryExecutor
 
     protected function delete()
     {
-        if (!isset($this->dsl['id']) and !isset($this->dsl['identifier'])) {
-            throw new \Exception("The 'id' or 'identifier' key is required to delete a content type group.");
+        $groupsCollection = $this->matchContentTypeGroups('delete');
+
+        $objectStateService = $this->repository->getContentTypeService();
+
+        foreach ($groupsCollection as $contentTypeGroup) {
+            $objectStateService->deleteContentTypeGroup($contentTypeGroup);
         }
 
-        $service = $this->repository->getContentTypeService();
-        if (isset($this->dsl['id'])) {
-            $group = $service->loadContentTypeGroup($this->dsl['id']);
-        } else {
-            $group = $service->loadContentTypeGroupByIdentifier($this->dsl['identifier']);
+        return $groupsCollection;
+    }
+
+    /**
+     * @param string $action
+     * @return ContentTypeGroupCollection
+     * @throws \Exception
+     */
+    protected function matchContentTypeGroups($action)
+    {
+        if (!isset($this->dsl['match'])) {
+            throw new \Exception("A match condition is required to $action an ObjectStateGroup.");
         }
 
-        $service->deleteContentTypeGroup($group);
+        $match = $this->dsl['match'];
 
-        return $group;
+        // convert the references passed in the match
+        foreach ($match as $condition => $values) {
+            if (is_array($values)) {
+                foreach ($values as $position => $value) {
+                    if ($this->referenceResolver->isReference($value)) {
+                        $match[$condition][$position] = $this->referenceResolver->getReferenceValue($value);
+                    }
+                }
+            } else {
+                if ($this->referenceResolver->isReference($values)) {
+                    $match[$condition] = $this->referenceResolver->getReferenceValue($values);
+                }
+            }
+        }
+
+        return $this->contentTypeGroupMatcher->match($match);
     }
 
     /**
@@ -64,6 +110,10 @@ class ContentTypeGroupManager extends RepositoryExecutor
                 case 'content_type_group_id':
                 case 'id':
                     $value = $object->id;
+                    break;
+                case 'content_type_group_identifier':
+                case 'identifier':
+                    $value = $object->identifier;
                     break;
                 default:
                     throw new \InvalidArgumentException('Content Manager does not support setting references for attribute ' . $reference['attribute']);
---
+++ Core/Matcher/ContentTypeGroupMatcher.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Kaliop\eZMigrationBundle\Core\Matcher;
+
+use Kaliop\eZMigrationBundle\API\Collection\ContentTypeGroupCollection;
+use eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroup;
+
+class ContentTypeGroupMatcher extends AbstractMatcher
+{
+    const MATCH_CONTENTTYPEGROUP_ID = 'contenttypegroup_id';
+    const MATCH_CONTENTTYPEGROUP_IDENTIFIER = 'contenttypegroup_identifier';
+
+    protected $allowedConditions = array(
+        self::MATCH_CONTENTTYPEGROUP_ID, self::MATCH_CONTENTTYPEGROUP_IDENTIFIER,
+        // aliases
+        'id', 'identifier'
+    );
+    protected $returns = 'ContentTypeGroup';
+
+    /**
+     * @param array $conditions key: condition, value: int / string / int[] / string[]
+     * @return ContentTypeGroupCollection
+     */
+    public function match(array $conditions)
+    {
+        return $this->matchContentTypeGroup($conditions);
+    }
+
+    /**
+     * @param int $groupId
+     * @return ContentTypeGroup
+     */
+    public function matchByKey($groupId)
+    {
+        return $this->findContentTypeGroupsById([$groupId])[$groupId];
+    }
+
+    /**
+     * @param array $conditions key: condition, value: int / string / int[] / string[]
+     * @return ContentTypeGroupCollection
+     */
+    public function matchContentTypeGroup($conditions)
+    {
+        $this->validateConditions($conditions);
+
+        foreach ($conditions as $key => $values) {
+
+            if (!is_array($values)) {
+                $values = array($values);
+            }
+
+            switch ($key) {
+                case 'id':
+                case self::MATCH_CONTENTTYPEGROUP_ID:
+                    return new ContentTypeGroupCollection($this->findContentTypeGroupsById($values));
+
+                case 'identifier':
+                case self::MATCH_CONTENTTYPEGROUP_IDENTIFIER:
+                    return new ContentTypeGroupCollection($this->findContentTypeGroupsByIdentifier($values));
+            }
+        }
+    }
+
+    /**
+     * @param int[] $contentTypeGroupIds
+     * @return ContentTypeGroup[]
+     */
+    protected function findContentTypeGroupsById(array $contentTypeGroupIds)
+    {
+        $contentTypeGroups = [];
+
+        foreach ($contentTypeGroupIds as $contentTypeGroupId) {
+            // return unique contents
+            $contentTypeGroup = $this->repository->getContentTypeService()->loadContentTypeGroup($contentTypeGroupId);
+            $contentTypeGroups[$contentTypeGroup->id] = $contentTypeGroup;
+        }
+
+        return $contentTypeGroups;
+    }
+
+        /**
+         * @param int[] $contentTypeGroupIdentifiers
+         * @return ContentTypeGroup[]
+         */
+    protected function findContentTypeGroupsByIdentifier(array $contentTypeGroupIdentifiers)
+    {
+        $contentTypeGroups = [];
+
+        foreach ($contentTypeGroupIdentifiers as $contentTypeGroupIdentifier) {
+            // return unique contents
+            $contentTypeGroup = $this->repository->getContentTypeService()->loadContentTypeGroupByIdentifier($contentTypeGroupIdentifiers);
+            $contentTypeGroups[$contentTypeGroup->id] = $contentTypeGroup;
+        }
+
+        return $contentTypeGroups;
+    }
+}
