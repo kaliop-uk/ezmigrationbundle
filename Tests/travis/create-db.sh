59880dbc8c5e550c166cde3b5da4b672dd23eb71
gggeek <ggiunta@kaliop.com>
2017-01-14 18:27:15 +0000

    etc

--- Tests/travis/create-db.sh
+++ Tests/travis/create-db.sh
@@ -19,5 +19,9 @@ if [ "$EZ_VERSION" = "ezplatform" ]; then mysql -u${DB_USER} ${DB_PWD} ${DB} < v
 if [ "$EZ_VERSION" = "ezplatform" ]; then mysql -u${DB_USER} ${DB_PWD} ${DB} < vendor/ezsystems/ezpublish-kernel/data/cleandata.sql; fi
 
 if [ "$INSTALL_TAGSBUNDLE" = "1" ]; then
-    mysql -u${DB_USER} ${DB_PWD} ${DB} < vendor/netgen/tagsbundle/Netgen/TagsBundle/Resources/sql/mysql/schema.sql
+    if [ -f vendor/netgen/tagsbundle/Netgen/TagsBundle/Resources/sql/mysql/schema.sql ]; then
+        mysql -u${DB_USER} ${DB_PWD} ${DB} < vendor/netgen/tagsbundle/Netgen/TagsBundle/Resources/sql/mysql/schema.sql
+    else
+        mysql -u${DB_USER} ${DB_PWD} ${DB} < vendor/netgen/tagsbundle/Resources/sql/mysql/schema.sql
+    fi
 fi
