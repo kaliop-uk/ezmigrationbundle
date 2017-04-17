a694dd451afaca235df5d20728930c9d75c463c3
gggeek <ggiunta@kaliop.com>
2017-04-17 19:01:09 +0100

    Final (?) fix for Travis tests

--- Tests/phpunit/0_CollectionsTest.php
+++ Tests/phpunit/0_CollectionsTest.php
@@ -10,6 +10,16 @@ use Kaliop\eZMigrationBundle\API\Value\MigrationStep;
  */
 class CollectionsTest extends CommandTest
 {
+    // phpunit compat layer: 4.8 => 6.x
+    public function expectException($exception)
+    {
+        if (method_exists($this, 'setExpectedException')) {
+            parent::setExpectedException($exception);
+        } else {
+            parent::expectException($exception);
+        }
+    }
+
     public function testValidElements1()
     {
         $collection = new MigrationStepsCollection(array(new MigrationStep('test')));
@@ -29,7 +39,7 @@ class CollectionsTest extends CommandTest
 
     public function testInvalidElements1()
     {
-        $this->setExpectedException('InvalidArgumentException');
+        $this->expectException('InvalidArgumentException');
         $collection = new MigrationStepsCollection(array(1));
     }
 
@@ -37,7 +47,7 @@ class CollectionsTest extends CommandTest
     {
         $collection = new MigrationStepsCollection(array());
 
-        $this->setExpectedException('InvalidArgumentException');
+        $this->expectException('InvalidArgumentException');
         $collection[] = 'not a content';
     }
 
@@ -45,7 +55,7 @@ class CollectionsTest extends CommandTest
     {
         $collection = new MigrationStepsCollection(array());
 
-        $this->setExpectedException('InvalidArgumentException');
+        $this->expectException('InvalidArgumentException');
         $collection['test'] = true;
     }
 
@@ -55,7 +65,7 @@ class CollectionsTest extends CommandTest
         $collection->exchangeArray(array(new MigrationStep('test2')));
         $this->assertEquals('test2', $collection[0]->type);
 
-        $this->setExpectedException('InvalidArgumentException');
+        $this->expectException('InvalidArgumentException');
         $collection->exchangeArray(array('hello'));
     }
 }
