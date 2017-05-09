bd6da47dd6dc41ce3ea064673154927a809b816e
gggeek <ggiunta@kaliop.com>
2017-05-09 23:31:29 +0100

    Add a bit more compat with v.3

---
+++ Core/StorageHandler/Database.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Kaliop\eZMigrationBundle\Core\StorageHandler;
+
+use Kaliop\eZMigrationBundle\Core\StorageHandler\Database\Migration;
+
+/**
+ * @deprecated Left in for backwards compatibility only
+ */
+class Database extends Migration
+{
+}
