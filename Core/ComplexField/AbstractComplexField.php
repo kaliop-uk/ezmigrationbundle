5d140667f45f4a24868203dc0b8b7407af0029ad
gggeek <ggiunta@kaliop.com>
2017-05-13 00:02:16 +0100

    bring back some classes for BC

---
+++ Core/ComplexField/AbstractComplexField.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Kaliop\eZMigrationBundle\Core\ComplexField;
+
+
+use Kaliop\eZMigrationBundle\Core\FieldHandler\AbstractFieldHandler;
+
+/**
+ * Kept around for BC, will be removed in a future release
+ * @deprecated
+ */
+abstract class AbstractComplexField extends AbstractFieldHandler
+{
+}
---
+++ Core/ComplexField/ComplexFieldManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kaliop\eZMigrationBundle\Core\ComplexField;
+
+use Kaliop\eZMigrationBundle\Core\FieldHandlerManager;
+
+/**
+ * Kept around for BC, will be removed in a future release
+ * @deprecated
+ */
+class ComplexFieldManager extends FieldHandlerManager
+{
+}
