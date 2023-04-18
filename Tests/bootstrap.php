79c69944b4d3e1f6f632ea5c2e79a99be4c29eff
gggeek <ggiunta@kaliop.com>
2023-04-18 16:27:48 +0000

    improve tests

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -7,4 +7,6 @@ if (!file_exists($file = __DIR__.'/../vendor/autoload.php') && !file_exists($fil
 
 $loader = require $file;
 
+/// @todo if the kernel is booted first with this variable set to false, by eg. running a cli command instead of phpunit,
+///       then it will be cached with the test config not loaded, which will make tests fail. How to prevent that?
 Kaliop\eZMigrationBundle\DependencyInjection\EzMigrationExtension::$loadTestConfig = true;
